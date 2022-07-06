165ced9bf9c68a9873b986c1a9ebd356e47477c3
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-07-06 12:30:09 +0200

    .

---
+++ src/components/Characters.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+
+
+
+const Characters = () => {
+
+
+
+
+
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log(data);
+    setCharacters(data.results);
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+
+
+
+  return <div >
+    {characters.map((character) => (
+
+      <div key={character.name}>{character.name}</div>
+
+
+    ))}
+
+    
+  </div>;
+};
+
+export default Characters;
